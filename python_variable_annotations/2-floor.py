63eedc2babfd33ae67ada6e160de140aab0477e9
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-23 11:16:58 -0400

    floor function for floats, returns int

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Type-annotated function add that takes float 'n'
+    and returns the floor of the float as int
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+        Returns floor of float 'n' as int
+    """
+
+    return math.floor(n)
