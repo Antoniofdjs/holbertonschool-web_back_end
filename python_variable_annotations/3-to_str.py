4b8da7b9fdc36a60102972ecd92f2088803265f8
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-23 11:31:54 -0400

    change float to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Type-annotated function add that takes float 'n'
+    and returns it as a str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        Returns float 'n' as str
+    """
+
+    return str(n)
