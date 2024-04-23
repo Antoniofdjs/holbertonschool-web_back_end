0e8bc503419976e5ac5a08329dd0b5d2245f9a29
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-23 11:03:30 -0400

    type_annotaded for strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Type-annotated function add that concats 'str1' and
+    float 'str2' as a single str
+"""
+
+
+def add(str1: str, str2: str) -> str:
+    """
+        Returns a str concat of str1 + str2
+    """
+    return str1 + str2
