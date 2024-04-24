276552697c6eeb6a93e1237f5a45adb282351ab5
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-24 14:07:44 -0400

    Pycode style

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,11 @@
 """
 from typing import Sequence, Any, Optional
 
+
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """
+        get frist element of sequence or return none
+    """
     if lst:
         return lst[0]
     else:
