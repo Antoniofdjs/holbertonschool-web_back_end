b06ccab90fef91124bedc21522edf8f958f70f8b
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-24 14:06:24 -0400

    pycode style

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+"""
+    Validation of task code in intranet
+"""
 from typing import Tuple, List, Any
 
+
 def zoom_array(lst: Tuple[Any], factor: int = 2) -> List[Any]:
+    """Repeats elements in a list 'factor' times.
+        Args:
+        lst: The input list.
+        factor (int, optional): Number of times to repeat elements
+
+    Returns:
+        List with elements repeated 'factor' times.
+  """
     zoomed_in: List[Any] = [
         item for item in lst
         for i in range(factor)
