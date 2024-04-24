fdaa951fa3dafbdfd4f9550e49a8df9492b8e14d
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-04-24 13:59:03 -0400

    advanceed tasks

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Get first element of sequence.
+"""
+from typing import Sequence, Any, Optional
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    if lst:
+        return lst[0]
+    else:
+        return None
