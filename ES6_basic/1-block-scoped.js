4a75d3c6fa7b1568d9f852c7a23e1608835719d5
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-05-06 18:01:51 -0400

    reverting

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,15 +1,10 @@
-/**
- * Represents a function that performs a task based on a boolean input.
- * @param {boolean} trueOrFalse - Determines whether to perform the task (true) or not (false).
- * @returns {Array<boolean>} An array containing two boolean values.
- */
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
 
   if (trueOrFalse) {
-    var innertask = true; // Variable used internally if trueOrFalse is true.
-    var innertask2 = false; // Another variable used internally if trueOrFalse is true.
+    var innertask = true;
+    var innertask2 = false;
   }
 
   return [task, task2];
