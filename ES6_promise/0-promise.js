18099b4d688d83f8a6980413cab590c6380a30ba
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-05-14 19:12:50 -0400

    adding a reject

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,10 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    resolve();
+    const data = 1;
+    if (data) {
+      resolve();
+    } else {
+      reject();
+    }
   });
 }
