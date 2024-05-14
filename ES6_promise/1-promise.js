64fbe4dc006a7af104675772079094be0b99918c
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-05-14 19:29:55 -0400

    using single quotes

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 const getFullResponseFromAPI = (success) => {
   const p = new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: "Success" });
+      resolve({ status: 200, body: 'Success'});
     } else {
-      const error = new Error("The fake API is not working currently");
+      const error = new Error('The fake API is not working currently');
       reject(error);
     }
   });
