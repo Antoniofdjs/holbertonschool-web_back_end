ddc86540a0e74cadd6ab853cadefd49c5c2b0f36
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-05-15 21:37:02 -0400

    to string for the error in reason

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,19 +1,21 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
-  return Promise.allSettled([promise1, promise2]).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
+  return Promise.all([promise1, promise2]).then((results) =>
+    results.map((result) => {
+      if (result.status === "fulfilled") {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      }
       return {
         status: result.status,
-        value: result.value,
+        value: result.reason,
       };
-    }
-    return {
-      status: result.status,
-      value: result.reason,
-    };
-  }));
+    })
+  );
 }
