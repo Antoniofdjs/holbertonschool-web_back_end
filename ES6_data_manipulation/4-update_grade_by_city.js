17769bbc99970fff5a6ea2d1630b69903a7fc319
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-05-20 17:22:05 -0400

    combining results

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,18 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  const filteredStudents = studentList.filter(
+    (student) => student.location === city
+  );
+  const studentsAndGrades = filteredStudents.map((student) => {
+    // Filter by student.id and grade.studentId
+    const studentGrade = newGrades.find(
+      (grade) => grade.studentId === student.id
+    );
+    // Found a grade that has the student id
+    if (studentGrade) {
+      return { ...student, grade: studentGrade.grade };
+    } else {
+      return { ...student, grade: 'N/A' };
+    }
+  });
+  return studentsAndGrades;
+}
